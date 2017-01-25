7b6d977f625b32814feb25114a2c2dcb3e7d091b
Trevor Renshaw <trevren11@gmail.com>
2017-01-25 09:29:55 -0700

    Add firebase

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB8IWqUnT6A5vzhrGOm56wUNMZvNBNjEIs",
+    authDomain: "movieq-d9325.firebaseapp.com",
+    databaseURL: "https://movieq-d9325.firebaseio.com",
+    storageBucket: "movieq-d9325.appspot.com",
+    messagingSenderId: "139642987985"
+};
+firebase.initializeApp(config);
